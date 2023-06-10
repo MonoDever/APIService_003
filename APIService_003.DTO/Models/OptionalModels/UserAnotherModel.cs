936c0e64f044aeae7a0b6411778724d8e4a0051f
Mono <siritaporn.mar@gmail.com>
2023-06-10 21:44:48 +0700

    first commit

---
+++ APIService_003.DTO/Models/OptionalModels/UserAnotherModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace APIService_003.DTO.Models.CommonModels
+{
+	public class UserAnotherModel
+	{
+		public UserAnotherModel()
+		{
+		}
+		public string? newPassword { get; set; }
+		public string? newSaltValue { get; set; }
+	}
+}
+
